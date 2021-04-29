8ef48dbe272028962fdfaef5c4564709895fc1d8
Anastasiia <a9n7a3815@gmail.com>
2021-04-29 09:26:56 -0400

    Create tests as a package

---
+++ tests1/run_all_tests.py
@@ -0,0 +1,9 @@
+from tests1.test_almost_win import*
+from tests1.test_check_draw import*
+from tests1.test_check_win import*
+from tests1.test_without_cross import*
+from tests1.test_zero_strategy import*
+
+
+if __name__ == '__main__':
+    unittest.main()
