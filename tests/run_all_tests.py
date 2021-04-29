c6da178df86440e5df5ac0a174d9732d56be20af
Anastasiia <a9n7a3815@gmail.com>
2021-04-29 09:53:01 -0400

    Rename tests1 to tests

---
+++ tests/run_all_tests.py
@@ -0,0 +1,9 @@
+from tests.test_almost_win import*
+from tests.test_check_draw import*
+from tests.test_check_win import*
+from tests.test_without_cross import*
+from tests.test_zero_strategy import*
+
+
+if __name__ == '__main__':
+    unittest.main()
